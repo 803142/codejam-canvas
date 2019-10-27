35dfd768a69d14b59fde335654411b6d866f737f
803142 <803142@gmail.com>
2019-10-27 19:45:56 +0300

    feat: add drawing image

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 class App {
 	constructor() {
-		this.activeMenuItem = "";
+		this.activeMenuItem = undefined;
 		this.mainfraim = new MainFrame();
 	}
 
@@ -21,7 +21,42 @@ class App {
 }
 
 function checkThisSize(size, address) {
-	alert(address);
+	const arr = dataImage(address);
+}
+function checkThisSizeImage(size, address) {
+	if (app.activeMenuItem && app.activeMenuItem != size) {
+		console.log(app.activeMenuItem);
+		app.activeMenuItem.classList.toggle("icon-blank");
+		app.activeMenuItem.classList.toggle("icon-check");
+	}
+	size.classList.toggle("icon-blank");
+	size.classList.toggle("icon-check");
+	app.activeMenuItem = size;
+	const canvasToDraw = document.querySelector(
+		'canvas[class="canvas__canvas-grid"]'
+	);
+	const ctx = canvasToDraw.getContext("2d");
+
+	const image = new Image(256, 256);
+	image.onload = drawImageActualSize;
+
+	image.src = address;
+
+	function drawImageActualSize() {
+		canvasToDraw.width = this.naturalWidth;
+		canvasToDraw.height = this.naturalHeight;
+		console.log(this.naturalWidth, this.naturalHeight);
+		ctx.drawImage(this, 0, 0);
+
+		ctx.drawImage(this, 0, 0, this.width, this.height);
+	}
+}
+async function dataImage(address) {
+	console.log(address);
+	let result = await fetch(address)
+		.then(response => response.json())
+		.catch(error => console.log("Request failed", error));
+	return result;
 }
 
 class MainFrame {
